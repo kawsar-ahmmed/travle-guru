4e416a0fa026da162723be30603dcd256ba775b6
kawsar-ahmmed <kawsarahmmedr@gmail.com>
2023-06-15 14:14:23 +0600

    Add property

--- src/Pages/CreateProperty/CreateProperty.js
+++ src/Pages/CreateProperty/CreateProperty.js
@@ -10,36 +10,40 @@ const CreateProperty = () => {
         const totalPrice = event.target.totalPrice.value;
         const rating = event.target.rating.value;
         const imgUrl = event.target.imgUrl.value;
-        const submit = { name, description, totalPrice, rating, imgUrl }
-        console.log(submit);
-        fetch('url', {
-            method: "POST",
+        const property = { name, description, totalPrice, rating, imgUrl };
+        console.log(property);
+        // url
+        const url = `http://localhost:5001/createproperty`;
+        fetch(url, {
+            method: 'POST',
             headers: {
-                'content-type': 'application/json'
+                'content-type': 'application/json',
             },
-            body: JSON.stringify()
+            body: JSON.stringify(property)
         })
-        .then(res=> res.json())
-        .then(data=> {
-            console.log(data)
-        })
+            .then(res => res.json())
+            .then(data => {
+                alert('User Added Successfully ')
+                console.log('success', data)
+                event.target.reset();
+            })
     }
-
     return (
-
         <div className='home-main justify-center h-screen flex items-center bg-slate-300'>
             <div className="form-contain container max-auto w-auto grid grid-cols">
                 <form onSubmit={handleProperty} className='flex flex-col'>
-                    <input type="text" name='name' placeholder='name' />
-                    <textarea type="text" name='description' placeholder='description' />
+                    <input type="text" name='name' placeholder='name' required />
+                    <textarea type="text" name='description' placeholder='description' required />
                     <div className="price-field flex gap-4">
-                        <input type="number" name='price' placeholder='price' />
-                        <input type="number" name='totalPrice' placeholder='total price' />
+                        <input className=' pr-[20px]' type="number" name="price" placeholder="$ Price" required inputMode="numeric" />
+                        <input className=' pr-[20px]' type="number" name="totalPrice" placeholder="Total Price" required />
                     </div>
-                    <input type="number" name='rating' placeholder='rating' />
-                    <input type="text" name='imgUrl' placeholder='image url' />
+                    <input type="number" name='rating' placeholder='rating' min="1" max="5" required />
+                    <input type="text" name='imgUrl' placeholder='image url' required />
                     <button className=' w-full py-[22px] bg-[#F9A51A] rounded'>Submit</button>
                 </form>
+
+
             </div>
         </div>
     );
